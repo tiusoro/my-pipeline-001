f5d2c0c84733c23c358f36aa53648ddcbbbcb646
Anthony Usoro <33380362+tiusoro@users.noreply.github.com>
2023-08-21 11:22:04 +0100

    Created script.groovy

---
+++ script.groovy
@@ -0,0 +1,13 @@
+def buildApp() {
+  echo 'building the application...'
+}
+
+def testApp() {
+  echo 'testing the application...'
+}
+
+def deployApp() {
+  echo 'building the application...'
+}
+
+return this
